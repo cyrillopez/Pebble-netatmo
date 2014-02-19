d23e1aa29f976180eb39964b8752d22c707a2b30
cyrillopez <cyril.lopez@gmail.com>
2014-02-19 22:25:21 +0100

    Version 1.01
    Toutes les variables sont désormais affichées

--- pebble-netatmo.php
+++ pebble-netatmo.php
@@ -1,60 +1,81 @@
 <?php
-    /* Script Cyril Lopez 
-    * Display weather informations from Netatmo station
-    * This script use the api from Netatmo
-    * Please configure it with Netatmo developper information
-    */
+    /* 
+    * Script by @Cyril Lopez 
+    * For display Netatmo informations on Pebble watch
+    * From Netatmo API
+    */ 
     
-    # Configure this
-    $app_id = 'YOUR_APP_ID';
-    $app_secret = 'YOUR_APP_SECRET';
-    $username = 'YOUR_USERNAME';
-    $password = 'YOUR_PASSWORD';
-    $device_id = "YOUR_DEVICE_ID"; //for internal module
-    $module_id = "YOUR_MODULE_ID"; //for external module
+// Mettre ce paramètre à true la première fois pour afficher la liste des stations et récupérer les adresses MAC pour $device_id et $module_id
+$display_list_devices = false; //true or false
+	
+// Nombre de minutes pour raffraichir les informations affichées
+$refresh_frequency = 30;
+	
+// Indiquez les informations après avoir créer une application sur http://dev.netatmo.com/dev/createapp
+$app_id = 'YOUR_APP_ID';
+$app_secret = 'YOUR_APP_SECRET';
+$username = 'YOUR_USERNAME';
+$password = 'YOUR_PASSWORD';
+$device_id = "YOUR_DEVICE_ID"; //Adresse Mac station
+$module_id = "YOUR_MODULE_ID"; //Adresse Mac module
     
-    $token_url = "https://api.netatmo.net/oauth2/token";
-	  $postdata = http_build_query(
-        array(
+   
+    
+// Ne rien toucher ci-dessous
+$postdata = http_build_query(
+	array(
             'grant_type' => "password",
             'client_id' => $app_id,
             'client_secret' => $app_secret,
             'username' => $username,
             'password' => $password
         )
-    );
+);
 
-    $opts = array('http' =>
+$opts = array('http' =>
     array(
         'method'  => 'POST',
         'header'  => 'Content-type: application/x-www-form-urlencoded',
         'content' => $postdata
     )
-    );
+);
 
-    $context  = stream_context_create($opts);
+$context  = stream_context_create($opts);
 
-    $response = file_get_contents($token_url, false, $context);
-    $params = null;
-    $params = json_decode($response, true);
+$response = file_get_contents($token_url, false, $context);
+$params = null;
+$params = json_decode($response, true);
 
-    $api_url_int = "https://api.netatmo.net/api/getmeasure?access_token=" . $params['access_token'] . "&device_id=" . $device_id . "&scale=max&type=Temperature,CO2,Humidity,Pressure,Noise&date_end=last";
-    $api_url_ext = "https://api.netatmo.net/api/getmeasure?access_token=" . $params['access_token'] . "&device_id=" . $device_id . "&module_id=" . $module_id . "&scale=max&type=Temperature,CO2,Humidity,Pressure,Noise&date_end=last";
+$api_liste = "https://api.netatmo.net/api/devicelist?access_token=" . $params['access_token'];
+$api_url_int = "https://api.netatmo.net/api/getmeasure?access_token=" . $params['access_token'] . "&device_id=" . $device_id . "&scale=max&type=Temperature,CO2,Humidity,Pressure,Noise&date_end=last";
+$api_url_ext = "https://api.netatmo.net/api/getmeasure?access_token=" . $params['access_token'] . "&device_id=" . $device_id . "&module_id=" . $module_id . "&scale=max&type=Temperature,CO2,Humidity,Pressure,Noise&date_end=last";
     
-    $data_int = json_decode(file_get_contents($api_url_int), true);
-    $data_ext = json_decode(file_get_contents($api_url_ext), true);
+if ($display_list_devices == true) 
+    	$data_liste = file_get_contents($api_liste);
+else {
+    	$data_int = json_decode(file_get_contents($api_url_int), true);
+    	$data_ext = json_decode(file_get_contents($api_url_ext), true);
+}
     
-    # Netatmo station datas
-    $temperature_int = $data_int['body'][0]['value'][0][0];
-    $co2_int = $data_int['body'][0]['value'][0][1];
-    $humidity_int = $data_int['body'][0]['value'][0][2];
-    $pressure_int = $data_int['body'][0]['value'][0][3];
-    $noise_int = $data_int['body'][0]['value'][0][4];
-    $temperature_ext = $data_ext['body'][0]['value'][0][0];
-    $humidity_ext = $data_ext['body'][0]['value'][0][2];
+// Configuration des variables 
+$temperature_int = $data_int['body'][0]['value'][0][0];
+$co2_int = $data_int['body'][0]['value'][0][1];
+$humidity_int = $data_int['body'][0]['value'][0][2];
+$pressure_int = $data_int['body'][0]['value'][0][3];
+$noise_int = $data_int['body'][0]['value'][0][4];
+$temperature_ext = $data_ext['body'][0]['value'][0][0];
+$humidity_ext = $data_ext['body'][0]['value'][0][2];
 	
-    # This is the Json line data display for external use
-    # You can configure this with informations that you want to extract
-    echo '{"content":"NETATMO\nInt : '.$temperature_int.' C\nExt : '.$temperature_ext.' C","refresh_frequency":30}';
-
+// Export données au format Json pour l'application Pebble Cards (http://keanulee.com/pebblecards/) sur la montre Pebble
+// Le format ci-dessous peut être modifié selon les informations souhaitées (voir variables disponibles ci-dessus)
+// Pour afficher toutes les informations dans Pebble Cards, maintenez le bouton select enfoncé
+if ($display_list_devices == true) {
+	echo 'Ci-dessous, la liste des stations paramétrées sur votre compte<br />';
+	echo 'Cette liste permet de récupérer les adresses Mac de vos stations et modules précédées de _id<br />';
+	echo 'Pensez à paramétrer la variable $display_list_devices à false une fois la configuration terminée<br />';
+	echo '<br />';
+	echo $data_liste;
+}
+else 
+	echo '{"content":"NETATMO\nInt : '.$temperature_int.' C\nExt : '.$temperature_ext.' C\nHum : '.$humidity_ext.' %\nCo2 : '.$co2_int.' ppm\nPres : '.$pressure_int.' mb\nSon : '.$noise_int.' db","refresh_frequency":'.$refresh_frequency.'}';
 ?>
